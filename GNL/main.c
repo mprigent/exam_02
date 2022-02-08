fc36080795e23f2a2de854715db994f80d1929ad
mprigent <77842628+mprigent@users.noreply.github.com>
2022-02-08 01:52:15 +0100

    Update main.c

--- GNL/main.c
+++ GNL/main.c
@@ -9,7 +9,7 @@ int main(int ac, char **av){
 	fd = open(av[1], O_RDONLY);
     while ((line = get_next_line(fd)) != 0)
 	{
-		printf("%s\n", line);
+		printf("%s", line);
 		free(line);
     }
 	close(fd);
